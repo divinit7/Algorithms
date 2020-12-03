5858b2454e296c0f0811affb1eb63c382785199a
Divyanshu Chauhan <chauhandivyanshu14@gmail.com>
2020-12-03 18:41:52 +0530

    more

---
+++ Java/BitwiseOp/src/PrintAbbreviations.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrintAbbreviations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(Arrays.toString(solution(s)));
+    }
+
+    private static String[] solution(String s) {
+        String[] arr = new String[(int) Math.pow(2, s.length())];
+        
+        return arr;
+    }
+}
